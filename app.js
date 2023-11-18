0443ba0a435071990797cdab31b2d2490b5b2e31
Akiranishimura <nissi20ch@gmail.com>
2023-11-19 02:34:11 +0900

    fix error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const db = require('./lib/DB/connectDB');
+// const db = require('./lib/DB/connectDB');
 const PORT = process.env.PORT || 3001;
 
 
